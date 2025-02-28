00654bbebf8f867e45836d574a087ae389aef7f9
Paul Walkington <paulwalkington@hotmail.co.uk>
2025-02-28 10:43:32 +0000

    first commit

--- variable.tf
+++ variable.tf
@@ -22,12 +22,12 @@ variable "az_count" {
 
 variable "app_image" {
     description = "Docker image to run in the ECS cluster"
-    default = "bradfordhamilton/crystal_blockchain:latest"
+    default = "nginxdemos/hello:latest"
 }
 
 variable "app_port" {
     description = "Port exposed by the docker image to redirect traffic to"
-    default = 3000
+    default = 80
 
 }
 
